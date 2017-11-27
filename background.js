c8ba9eb8ed864d760ff89149a4c4a6da581403e6
Tyler Maciaszek <tmaciaszek10@gmail.com>
2017-11-27 17:03:41 -0500

    removed useless focus variable and associated setting

--- background.js
+++ background.js
@@ -3,12 +3,13 @@ var images = {}
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.name == 'currScreenshot') {
-        chrome.tabs.captureVisibleTab(null, null, function(data) {
-          console.log(data)
-            //image = new Image()
-            //image.src = data
-            sendResponse({newscreenshot: data, imageDic: images});
+      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+          currURL = tabs[0].url
+          chrome.tabs.captureVisibleTab(null, null, function(data) {
+            console.log(data)
+              sendResponse({newscreenshot: data, imageDic: images, currURL: currURL});
         });
+      });
     }
     return true;
 });
--- content.js
+++ content.js
@@ -1,17 +1,11 @@
-var focus = true;
 window.onfocus = function(){
-  if(!focus){
    chrome.extension.sendMessage({name: 'currScreenshot'}, function(response) {
-     var diff = resemble(response.newscreenshot).compareTo(response.imageDic['http://www.example.com/']).ignoreColors().onComplete(function (data){
-       console.log(data);
-     });
+     if(response.currURL in response.imageDic){
+       var diff = resemble(response.newscreenshot).compareTo(response.imageDic['http://www.example.com/']).ignoreColors().onComplete(function (data){
+         console.log(data);
+       });
+     }else{
+       console.log("no image taken yet")
+     }
    });
-
-
-  }
-  focus = true;
 }
-
-window.onblur = function() {
-    focus = false;
-};
