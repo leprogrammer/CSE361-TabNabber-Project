a85b0cf5dbeed8c645b14521493eb0dce934f51c
Tejas Prasad <tejas.prasad07@gmail.com>
2017-11-27 23:35:26 -0500

    Canvas works. Need to test if statement of drawing only when mismatch is greater than 0.

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 window.onfocus = function(){
    chrome.extension.sendMessage({name: 'currScreenshot'}, function(response) {
      if(response.currURL in response.imageDic){
-       var diff = resemble(response.newscreenshot).compareTo(response.imageDic[response.currURL]).ignoreColors().onComplete(function (data){
+       var diff = resemble(response.newscreenshot).compareTo(response.imageDic[response.currURL]).ignoreLess().scaleToSameSize().onComplete(function (data){
          console.log(data);
-         var divElement = document.createElement("div");
+         /*var divElement = document.createElement("div");
          divElement.id = "1337Overlay";
          divElement.style.display = "block";
          divElement.style.position = "fixed";
@@ -17,12 +17,33 @@ window.onfocus = function(){
          divElement.style.zIndex = "2";
          divElement.onclick = function(){
            document.getElementById("1337Overlay").style.display = "none";
-         };
+         };*/
+         if(data.misMatchPercentage > 0){
+           var canvasElement = document.createElement("canvas");
+           canvasElement.id = "1337Overlay";
+           canvasElement.style.display = "block";
+           canvasElement.style.position = "fixed";
+           canvasElement.style.height = "100%";
+           canvasElement.style.width = "100%";
+           canvasElement.style.top = "0";
+           canvasElement.style.left = "0";
+           canvasElement.style.right = "0";
+           canvasElement.style.bottom = "0";
+           canvasElement.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+           canvasElement.style.zIndex = "2";
+           canvasElement.onclick = function(){
+             document.getElementById("1337Overlay").style.display = "none";
+           };
+           var context = canvasElement.getContext("2d");
 
-         var differenceImage = new Image();
-         differenceImage.src = data.getImageDataUrl();
-         //divElement.innerHTML = differenceImage;
-         document.body.appendChild(divElement);
+           var differenceImage = new Image();
+           differenceImage.onload = function(){
+             context.drawImage(differenceImage, canvasElement.width / 2 - differenceImage.width / 2, canvasElement.height / 2 - differenceImage.height / 2);
+           };
+           differenceImage.src = data.getImageDataUrl();
+           //divElement.innerHTML = differenceImage;
+           document.body.appendChild(canvasElement);
+        }
        });
      }else{
        console.log("no image taken yet")
