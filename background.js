5bedf55da971d5aa5318f1c0550ee2a8d04c1693
Tejas Prasad <tejas.prasad07@gmail.com>
2017-12-01 11:25:19 -0500

    Added code for user feedback via color.

--- background.js
+++ background.js
@@ -13,7 +13,27 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     }
     else if (request.mismatch != null){
       chrome.tabs.getSelected(null, function(tabs){
+        var colorString = "#FFFFFF";
+        var mismatchNumber = parseFloat(request.mismatch);
+
+        if(mismatchNumber == 0){
+          colorString = "#00FF00";
+        }
+        else if(mismatchNumber > 0 && mismatchNumber <= 10){
+          colorString = "#00B000";
+        }
+        else if(mismatchNumber > 10 && mismatchNumber <= 30){
+          colorString = "#0000F0";
+        }
+        else if(mismatchNumber > 30 && mismatchNumber <= 50){
+          colorString = "#F000F0";
+        }
+        else if(mismatchNumber > 50 && mismatchNumber <= 100){
+          colorString = "#F00000";
+        }
+
         chrome.browserAction.setBadgeText({text: String(request.mismatch), tabId: tabs.id});
+        chrome.browserAction.setBadgeBackgroundColor({color: String(colorString), tabId: tabs.id});
       });
     }
     return true;
